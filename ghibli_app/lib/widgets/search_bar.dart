dc2f26bb5e652294c4753a1832a5e5169ef4ddab
GHOSTVCL <joanetgiolet@gmail.com>
2023-12-27 19:26:26 +0100

    More advances in screen species

--- ghibli_app/lib/widgets/search_bar.dart
+++ ghibli_app/lib/widgets/search_bar.dart
@@ -11,28 +11,27 @@ class _SearchBarWidgetState extends State<SearchBarWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50,
-      margin: const EdgeInsets.symmetric(horizontal: 15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(45),
-        color: Colors.white70,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.6),
-            offset: const Offset(
-              0.0,
-              10.0,
+        height: 50,
+        margin: const EdgeInsets.symmetric(horizontal: 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(45),
+          color: Colors.white70,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.6),
+              offset: const Offset(
+                0.0,
+                10.0,
+              ),
+              blurRadius: 10.0,
+              spreadRadius: -6.0,
             ),
-            blurRadius: 10.0,
-            spreadRadius: -6.0,
-          ),
-        ],
-      ),
-      child: TextField(
-  onChanged: (text) {
-    debugPrint("text = '$text}'");
-  },
-)
-    );
+          ],
+        ),
+        child: TextField(
+          onChanged: (text) {
+            debugPrint("text = '$text}'");
+          },
+        ));
   }
 }
--- ghibli_app/lib/widgets/species_card_widget.dart
+++ ghibli_app/lib/widgets/species_card_widget.dart
@@ -16,6 +16,111 @@ class SpeciesCard extends StatefulWidget {
 class _SpeciesCardState extends State<SpeciesCard> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        width: 200,
+        height: 180,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 83, 126, 83),
+          border: Border.all(
+            width: 5,
+            color: Color.fromARGB(255, 52, 78, 52),
+          ),
+        ),
+        child: Align(
+          alignment: Alignment.center,
+          child: Row(
+            children: [
+              Image.asset(
+                "assets/images/logo_ghibli.png",
+              ),
+              Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: Column(
+                    children: [
+                      Text(
+                        widget.species.name,
+                        style: const TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Row(children: [
+                        const Text(
+                          "Classification: ",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 2, 2, 2),
+                          ),
+                        ),
+                        Text(
+                          widget.species.classification,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          ),
+                        ),
+                      ]),
+                      Row(children: [
+                        const Text(
+                          "Eye colours: ",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 2, 2, 2),
+                          ),
+                        ),
+                        Text(
+                          widget.species.eye_colors,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          ),
+                        ),
+                      ]),
+                      Row(children: [
+                        const Text(
+                          "Hair colours: ",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 2, 2, 2),
+                          ),
+                        ),
+                        Text(
+                          widget.species.hair_colors,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          ),
+                        ),
+                      ]),
+                      Row(children: [
+                        const Text(
+                          "Film: ",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 2, 2, 2),
+                          ),
+                        ),
+                        Text(
+                          widget.species.film_name,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          ),
+                        ),
+                      ]),
+                    ],
+                  )),
+            ],
+          ),
+        ));
   }
 }
