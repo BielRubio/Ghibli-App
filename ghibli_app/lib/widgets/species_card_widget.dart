b2420c26db1f78b8602d68bf69985e7d86675efa
GHOSTVCL <joanetgiolet@gmail.com>
2023-12-27 19:51:31 +0100

    Some more changes, not definitive

--- ghibli_app/lib/widgets/species_card_widget.dart
+++ ghibli_app/lib/widgets/species_card_widget.dart
@@ -17,110 +17,119 @@ class _SpeciesCardState extends State<SpeciesCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        alignment: Alignment.center,
-        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-        width: 200,
-        height: 180,
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 83, 126, 83),
-          border: Border.all(
-            width: 5,
-            color: Color.fromARGB(255, 52, 78, 52),
-          ),
+      alignment: Alignment.center,
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      width: 200,
+      height: 220,
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 176, 182, 178),
+        border: Border.all(
+          width: 5,
+          color: Color.fromARGB(255, 70, 71, 71),
         ),
-        child: Align(
-          alignment: Alignment.center,
-          child: Row(
-            children: [
-              Image.asset(
-                "assets/images/logo_ghibli.png",
-              ),
-              Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  child: Column(
-                    children: [
-                      Text(
-                        widget.species.name,
-                        style: const TextStyle(
-                          fontSize: 22,
+      ),
+      child: Align(
+        alignment: Alignment.center,
+        child: Row(
+          children: [
+            Image.asset(
+              "assets/images/logo_ghibli.png",
+            ),
+            Expanded(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.species.name,
+                      style: const TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Row(children: [
+                      const Text(
+                        "Classification: ",
+                        style: TextStyle(
+                          fontSize: 16,
                           fontWeight: FontWeight.bold,
-                          color: Colors.black,
+                          color: Color.fromARGB(255, 0, 0, 0),
                         ),
                       ),
-                      const SizedBox(height: 5),
-                      Row(children: [
-                        const Text(
-                          "Classification: ",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 2, 2, 2),
-                          ),
+                      Text(
+                        widget.species.classification,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Color.fromARGB(255, 0, 0, 0),
                         ),
-                        Text(
-                          widget.species.classification,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Color.fromARGB(255, 0, 0, 0),
-                          ),
+                      ),
+                    ]),
+                    Row(children: [
+                      const Text(
+                        "Eye colours: ",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 0, 0, 0),
                         ),
-                      ]),
-                      Row(children: [
-                        const Text(
-                          "Eye colours: ",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 2, 2, 2),
-                          ),
-                        ),
-                        Text(
+                      ),
+                      Flexible(
+                        child: Text(
                           widget.species.eye_colors,
                           style: const TextStyle(
                             fontSize: 16,
                             color: Color.fromARGB(255, 0, 0, 0),
                           ),
                         ),
-                      ]),
-                      Row(children: [
-                        const Text(
-                          "Hair colours: ",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 2, 2, 2),
-                          ),
+                      ),
+                    ]),
+                    Row(children: [
+                      const Text(
+                        "Hair colours: ",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 0, 0, 0),
                         ),
-                        Text(
+                      ),
+                      Flexible(
+                        child: Text(
                           widget.species.hair_colors,
                           style: const TextStyle(
                             fontSize: 16,
                             color: Color.fromARGB(255, 0, 0, 0),
                           ),
                         ),
-                      ]),
-                      Row(children: [
-                        const Text(
-                          "Film: ",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 2, 2, 2),
-                          ),
+                      ),
+                    ]),
+                    Row(children: [
+                      const Text(
+                        "Film: ",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 0, 0, 0),
                         ),
-                        Text(
-                          widget.species.film_name,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Color.fromARGB(255, 0, 0, 0),
-                          ),
+                      ),
+                      Text(
+                        widget.species.film_name,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Color.fromARGB(255, 0, 0, 0),
                         ),
-                      ]),
-                    ],
-                  )),
-            ],
-          ),
-        ));
+                      ),
+                    ]),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
