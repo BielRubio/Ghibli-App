49d106ac6cfca0289b32e5b2704f377887278dc6
GHOSTVCL <joanetgiolet@gmail.com>
2024-01-14 01:12:05 +0100

    Advancing in dark/light

--- ghibli_app/lib/screens/settings_screen.dart
+++ ghibli_app/lib/screens/settings_screen.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: deprecated_member_use, unused_local_variable
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ghibli_app/model/themeprovider.dart';
@@ -15,83 +16,93 @@ class _SettingsScreenState extends State<SettingsScreen> {
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context, listen: false);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-        actions: <Widget>[
-          // Dark Mode Toggle Icon
-          IconButton(
-            icon: Icon(
-              themeProvider.themeMode == ThemeMode.dark
-                  ? Icons.dark_mode
-                  : Icons.light_mode,
+        appBar: AppBar(
+          title: const Text('Settings',
+              textScaleFactor: 1.8, style: TextStyle(fontFamily: 'Ghibli')),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 125, 189, 125),
+          toolbarHeight: 80,
+          actions: <Widget>[
+            Consumer<ThemeProvider>(
+              builder: (context, themeProvider, child) {
+                return IconButton(
+                  icon: Icon(
+                    themeProvider.themeMode == ThemeMode.dark
+                        ? Icons.light_mode
+                        : Icons.dark_mode,
+                  ),
+                  onPressed: () {
+                    themeProvider
+                        .toggleTheme(themeProvider.themeMode != ThemeMode.dark);
+                  },
+                );
+              },
             ),
-            onPressed: () {
-              // Toggle the theme mode
-              themeProvider
-                  .toggleTheme(themeProvider.themeMode != ThemeMode.dark);
-            },
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Container(
-            margin: const EdgeInsets.all(25),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "ACCOUNT",
-                  style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 84, 84, 84)),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                SettingsParameterToggle(label: "Private Account", toggle: true),
-                SettingsParameterToggle(label: "Delete Account", toggle: false),
-                // SizedBox(
-                //   height: 40,
-                // ),
-                SizedBox(
-                  height: 70,
-                ),
-                Text(
-                  "NOTIFICATIONS",
-                  style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 84, 84, 84)),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                SettingsParameterToggle(label: "New Deals", toggle: true),
-                SettingsParameterToggle(label: "Friend Requests", toggle: true),
-                SettingsParameterToggle(
-                    label: "My Activity Likes", toggle: true),
-                SizedBox(
-                  height: 70,
-                ),
-                Text(
-                  "OTHER",
-                  style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 84, 84, 84)),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                SettingsParameterToggle(label: "Contact Us", toggle: false),
-              ],
-            ),
-          ),
+          ],
         ),
-      ),
-      backgroundColor: const Color.fromARGB(255, 228, 228, 228),
-    );
+        body: Container(
+          color: Theme.of(context).scaffoldBackgroundColor,
+          child: SingleChildScrollView(
+            child: Center(
+              child: Container(
+                margin: const EdgeInsets.all(25),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "ACCOUNT",
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 84, 84, 84)),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    SettingsParameterToggle(
+                        label: "Private Account", toggle: true),
+                    SettingsParameterToggle(
+                        label: "Delete Account", toggle: false),
+                    // SizedBox(
+                    //   height: 40,
+                    // ),
+                    SizedBox(
+                      height: 70,
+                    ),
+                    Text(
+                      "NOTIFICATIONS",
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 84, 84, 84)),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    SettingsParameterToggle(label: "New Deals", toggle: true),
+                    SettingsParameterToggle(
+                        label: "Friend Requests", toggle: true),
+                    SettingsParameterToggle(
+                        label: "My Activity Likes", toggle: true),
+                    SizedBox(
+                      height: 70,
+                    ),
+                    Text(
+                      "OTHER",
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 84, 84, 84)),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    SettingsParameterToggle(label: "Contact Us", toggle: false),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
